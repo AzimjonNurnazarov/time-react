edf67f7001c7becf3602c964805a4e011b95d4e5
AzimjonNurnazarov <azimn1993@gmail.com>
2024-12-24 09:15:15 +0500

    time

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-useless-constructor */
+import React, { Component } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Controls from "./Components/Controls";
+import Header from "./Components/Header";
+
+export class App extends Component {
+  //   constructor(props) {
+  //     super(props);
+  //   }
+
+  render() {
+    return (
+      <div className="container">
+        <Header hour={10} minute={60} second={60} />
+        <Controls />
+      </div>
+    );
+  }
+}
+
+export default App;
